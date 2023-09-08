a418e12288910adb6b1162c93505872e25733d1a
Dirk Michel <michel.dirk@googlemail.com>
2023-09-08 14:55:57 +0100

    all

--- tests/unit/test_cdk_dev_stack.py
+++ tests/unit/test_cdk_dev_stack.py
@@ -1,6 +1,6 @@
 import aws_cdk as core
 import aws_cdk.assertions as assertions
-from cdk_dev.cdk_dev_stack import CdkDevStack
+from cdk_dev.sbom_stack import CdkDevStack
 
 
 def test_sqs_queue_created():
